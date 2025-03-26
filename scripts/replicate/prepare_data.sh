0ce25bed0580ffd113a0c505c47fee8e7d1c3ff7
LChenGit <lechen@iastate.edu>
2025-03-26 16:56:47 -0500

    update prepare_data.sh to pass the path parameter

--- scripts/replicate/prepare_data.sh
+++ scripts/replicate/prepare_data.sh
@@ -3,7 +3,9 @@
 # python scripts/data/deepscaler_dataset.py --num_tokens 2048
 # python scripts/data/deepscaler_dataset.py --num_tokens 3600
 
-python scripts/data/deepscaler_dataset.py --num_tokens -512 --local_dir ~/deepscaler/data_max
+python scripts/data/deepscaler_dataset.py --num_tokens -512 --local_dir "$1"
+
+# python scripts/data/deepscaler_dataset.py --num_tokens -512 --local_dir ~/deepscaler/data_max
 # python scripts/data/deepscaler_dataset.py --num_tokens -1024 --local_dir ~/deepscaler/data_max
 # python scripts/data/deepscaler_dataset.py --num_tokens -2048 --local_dir ~/deepscaler/data_max
 # python scripts/data/deepscaler_dataset.py --num_tokens -3600 --local_dir ~/deepscaler/data_max
